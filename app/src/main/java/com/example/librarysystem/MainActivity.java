806a53888610a5527724da51b4fc5a8750953bbe
johnsmith868 <john@john.com>
2020-05-03 16:28:12 +0800

    add material component

--- app/src/main/java/com/example/librarysystem/MainActivity.java
+++ app/src/main/java/com/example/librarysystem/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.librarysystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLoginButton = findViewById(R.id.btnLogin);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snackbar = Snackbar.make(v,"You click the button",Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });
+
     }
 }
