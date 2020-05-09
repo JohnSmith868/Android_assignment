e96cc7677143382f370a6f12703d7e19f67562e7
johnsmith868 <john@john.com>
2020-05-09 14:45:29 +0800

    fix gps-> back to home intent will crash

--- app/src/main/java/com/example/librarysystem/BranchesMapActivity.java
+++ app/src/main/java/com/example/librarysystem/BranchesMapActivity.java
@@ -5,29 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.awareness.snapshot.LocationResponse;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import org.json.JSONObject;
+import org.w3c.dom.ls.LSOutput;
 
 public class BranchesMapActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap map;
     Location currentLocation;
+    Marker currentlocationMarker;
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
 
@@ -41,10 +62,12 @@ public class BranchesMapActivity extends AppCompatActivity implements OnMapReady
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(4*1000);
-        locationRequest.setFastestInterval(2*1000);
+        locationRequest.setInterval(20*1000);
+        locationRequest.setFastestInterval(20*1000);
 
         fetchLastLocation();
+
+
     }
 
     private void fetchLastLocation() {
@@ -53,22 +76,69 @@ public class BranchesMapActivity extends AppCompatActivity implements OnMapReady
                     {Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_LOCATION);
             return;
         }
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest,new LocationCallback(){
+
+        LocationSettingsRequest locationSettingsRequest = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest).build();
+        SettingsClient client = LocationServices.getSettingsClient(this);
+
+        Task<LocationSettingsResponse> locationSettingsRequestTask = client.checkLocationSettings(locationSettingsRequest);
+
+
+        locationSettingsRequestTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
             @Override
-            public void onLocationResult(LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-                if(locationResult.getLastLocation() != null){
-                    currentLocation = locationResult.getLastLocation();
-                    Toast.makeText(getApplicationContext(),currentLocation.getLatitude()+""+currentLocation.getLongitude(),Toast.LENGTH_LONG).show();
-                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.branches_map);
-                    supportMapFragment.getMapAsync(BranchesMapActivity.this);
-                }
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest,new LocationCallback(){
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        if(locationResult.getLastLocation() != null ){
+                            currentLocation = locationResult.getLastLocation();
+                            Toast.makeText(getApplicationContext(),currentLocation.getLatitude()+""+currentLocation.getLongitude(),Toast.LENGTH_LONG).show();
+                            SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.branches_map);
+                            if(supportMapFragment!=null){
+                                supportMapFragment.getMapAsync(BranchesMapActivity.this);
+                            }
+
+
+
+                        }
+                    }
+
+                }, Looper.myLooper());
+
             }
-        },getMainLooper());
+        });
+        locationSettingsRequestTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(e instanceof ResolvableApiException){
+                    ResolvableApiException apiException = (ResolvableApiException)e;
+                    try {
+                        apiException.startResolutionForResult(BranchesMapActivity.this, 1234);
+                    } catch (IntentSender.SendIntentException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
+
+
+
+
 
 
     }
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("branchensmap leavbe","leving");
+        fusedLocationProviderClient.removeLocationUpdates(new LocationCallback());
+
+    }
 
     @Override
     protected void onResume() {
@@ -78,14 +148,58 @@ public class BranchesMapActivity extends AppCompatActivity implements OnMapReady
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         map = googleMap;
+        if(currentlocationMarker!=null){
+            currentlocationMarker.remove();
+        }
 
         float zoomLevel = 16.0f;
-        LatLng sportground = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(sportground).title("this is sport ground");
-        map.animateCamera(CameraUpdateFactory.newLatLng(sportground));
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(sportground,zoomLevel));
-        map.addMarker(markerOptions);
+        LatLng sportground = new LatLng(22.309045,114.178121);
+
+        LatLng currentLocationLatLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(currentLocationLatLng).title("this is sport ground");
+
+        map.animateCamera(CameraUpdateFactory.newLatLng(currentLocationLatLng));
+//        map.animateCamera(CameraUpdateFactory.newLatLngZoom(sportground,zoomLevel));
+         currentlocationMarker = map.addMarker(markerOptions);
+
+//
+//         String url = getUrl(currentLocationLatLng,sportground,"driv");
+//         RequestQueue queue = Volley.newRequestQueue(this);
+//        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.POST, url, null,
+//                new Response.Listener<JSONObject>()
+//                {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        // display response
+//                        Log.d("Response", response.toString());
+//                    }
+//                },
+//                new Response.ErrorListener()
+//                {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.d("Error.Response", error.toString());
+//                    }
+//                }
+//        );
+//
+//        queue.add(getRequest);
+
+
+
+    }
+
+    private String getUrl(LatLng from, LatLng to, String directionMode) {
+        String str_from = "origin="+from.latitude+","+from.longitude;
+        String str_to = "destination="+to.latitude+","+to.longitude;
+        String mode = "mode="+directionMode;
+        String parameters = str_from+"&"+str_to+"&"+mode;
+        String outputformat = "json";
+        String url = "https://maps.googleapis.com/maps/api/directions/"+outputformat+"?"+parameters+"&key="+getString(R.string.map_api_key);
+        return url;
+
     }
 
     @Override
